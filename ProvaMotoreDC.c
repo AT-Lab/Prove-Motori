9e3bd70d8367f1d3c8e3618de8454ad280f3750a
pazpi <pasettodavide@gmail.com>
2015-11-05 17:27:03 +0100

    Aggiunto supporto ENABLE

--- ProvaMotoreDC.c
+++ ProvaMotoreDC.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-#define SW1 0 
-#define SW2 7
-#define DIRA 5 
-#define DIRB 3
-#define EN 
+#define SW1 7 
+#define SW2 0 
+#define DIRA 5 //indietro
+#define DIRB 3 //avanti
+#define EN 6 //enable
 
 int main(void){
 	printf("Prova Motori 0: Prova Avanti-Retro di un motore in DC\n");
@@ -23,20 +23,22 @@ int main(void){
 
 	digitalWrite(DIRA, 0);
 	digitalWrite(DIRB, 0);
-	digitalWrite(EN, 0);
+	/* digitalWrite(EN, 0); */
 
 	while(1){
 		if(digitalRead(SW1==0)){
+	        digitalWrite(EN, 1);
 			digitalWrite(DIRA, 1);
 			digitalWrite(DIRB, 0);
 			printf("Motore avanti\n");
-			delay(10);
+			delay(50);
 		}
 		if(digitalRead(SW2==0)){
+	        digitalWrite(EN, 1);
 			digitalWrite(DIRA, 0);
 			digitalWrite(DIRB, 1);
 			printf("Motore inditro\n");
-			delay(10);
+			delay(50);
 		}
 		delay(10);
 	}
